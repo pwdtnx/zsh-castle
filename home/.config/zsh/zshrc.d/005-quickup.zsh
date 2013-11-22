1c17706f447bd91dedcc174014e49b39283c8853
Λlisue <lambdalisue@hashnote.net>
2013-11-23 05:57:12 +0900

    Fix cdup issue

--- home/.config/zsh/zshrc.d/005-quickup.zsh
+++ home/.config/zsh/zshrc.d/005-quickup.zsh
@@ -1,17 +1,39 @@
 # cd up with ^^
 cdup() {
-    echo
-    cd ..
-    zle reset-prompt
+    if [ -z "$BUFFER" ]; then
+        echo
+        cd ../
+        if type precmd > /dev/null 2>&1; then
+            precmd
+        fi
+        local precmd_func
+        for precmd_func in $precmd_functions; do
+            $precmd_func
+        done
+        zle reset-prompt
+    else
+        zle self-insert '^^'
+    fi
 }
 zle -N cdup
 bindkey '\^\^' cdup
 
 # popd up with [[
 function call_popd() {
-    echo
-    popd
-    zle reset-prompt
+    if [ -z "$BUFFER" ]; then
+        echo
+        popd
+        if type precmd > /dev/null 2>&1; then
+            precmd
+        fi
+        local precmd_func
+        for precmd_func in $precmd_functions; do
+            $precmd_func
+        done
+        zle reset-prompt
+    else
+        zle self-insert '[['
+    fi
 }
 zle -N call_popd
 bindkey '[[' call_popd

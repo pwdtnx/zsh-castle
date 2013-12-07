53bba77ec25268589072bc98c57955a46b5f1113
Tanix <pwdtnx@gmail.com>
2013-12-07 22:50:42 +0900

    track original

--- home/.config/zsh/zshrc.d/999-alias.zsh
+++ home/.config/zsh/zshrc.d/999-alias.zsh
@@ -14,19 +14,5 @@ if [[ -d "$HOME/.anyenv/bin" ]]; then
     eval "$(anyenv init -)"
 fi
 
-# Texlive 2013
-TEXLIVE="/usr/local/texlive/2013"
-if [[ -d "$TEXLIVE" ]]; then
-    TEXLIVE_BIN_x86="$TEXLIVE/bin/i386-linux"
-    TEXLIVE_BIN_x64="$TEXLIVE/bin/x86_64-linux"
-    if [[ -d "$TEXLIVE_BIN_x86" ]]; then
-        export PATH="$TEXLIVE_BIN_x87:$PATH"
-    elif [[ -d "$TEXLIVE_BIN_x64" ]]; then
-        export PATH="$TEXLIVE_BIN_x64:$PATH"
-    fi
-    export MANPATH="$TEXLIVE/texmf-dist/doc/man:$MANPATH"
-    export INFOPATH="$TEXLIVE/texmf-dist/doc/info:$INFOPATH"
-fi
-
 # Load local configures
 [[ -f ~/.profile ]] && source ~/.profile
